54977cc0c35d7610b6afb8d46a8788f70ab9b40a
MaxGuidry <mguidry7@outlook.com>
2019-06-17 08:23:57 -0500

    stuff

---
+++ Assets/Scripts/DepthPenetrationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthPenetrationBehaviour : MonoBehaviour
+{
+    public float pentrationPercent;
+    public int numVertex;
+    public int vertsInside;
+    public int VertexPercentageCheck;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var meshRef = other.GetComponent<MeshFilter>().mesh;
+        numVertex = meshRef.vertexCount / VertexPercentageCheck;
+        vertsInside = 0;
+        for (int i = 0; i < numVertex * VertexPercentageCheck; i += VertexPercentageCheck)
+        {
+            if (this.GetComponent<Collider>().bounds.Contains(meshRef.vertices[i] + other.transform.position))
+            {
+                vertsInside++;
+            }
+        }
+        pentrationPercent = ((float)vertsInside / (float)numVertex) * 100;
+        if (pentrationPercent > 99)
+            Debug.Log("GOAL");
+    }
+}
